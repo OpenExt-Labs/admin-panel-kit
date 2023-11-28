28404eb1d2206ae9f8772b241a440aa4effebbc4
sanglq3 <sanglq3@vng.com.vn>
2023-11-28 16:11:13 +0700

    update get html conent

--- src/app/editor/tiptap-editor.tsx
+++ src/app/editor/tiptap-editor.tsx
@@ -1,7 +1,9 @@
 'use client'
-import { Editor, EditorContent, Extensions, useEditor } from '@tiptap/react';
+import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import MenuBar from './menu-bar';
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
 
 
 
@@ -9,12 +11,16 @@ const defaultValue =
   '<h2 style="text-align: center;">Welcome to Mantine rich text editor</h2><p><code>RichTextEditor</code> component focuses on usability and is designed to be as simple as possible to bring a familiar editing experience to regular users. <code>RichTextEditor</code> is based on <a href="https://tiptap.dev/" rel="noopener noreferrer" target="_blank">Tiptap.dev</a> and supports all of its features:</p><ul><li>General text formatting: <strong>bold</strong>, <em>italic</em>, <u>underline</u>, <s>strike-through</s> </li><li>Headings (h1-h6)</li><li>Sub and super scripts (<sup>&lt;sup /&gt;</sup> and <sub>&lt;sub /&gt;</sub> tags)</li><li>Ordered and bullet lists</li><li>Text align&nbsp;</li><li>And all <a href="https://tiptap.dev/extensions" target="_blank" rel="noopener noreferrer">other extensions</a></li></ul>';
 
 const TipTapEditor = () => {
+  const [httmlContent, setHtmlContent] = useState(defaultValue)
+  const [isShowContent, setIsShowContent] = useState(false)
+
   const editor = useEditor({
     extensions: [
       StarterKit,
     ],
     content: defaultValue,
     onUpdate: ({ editor }) => {
+      setHtmlContent(editor.getHTML())
     },
     editorProps: {
       attributes: {
@@ -33,6 +39,16 @@ const TipTapEditor = () => {
         )
       }
       <EditorContent editor={editor} className='border border-gray-400 mt-4 rounded-md' />
+      <Button className='mt-4' onClick={() => setIsShowContent(!isShowContent)}>Toggle HTML</Button>
+      {
+        isShowContent && (
+          <div className='mt-4'>
+            <div className='border border-gray-400 mt-4 rounded-md p-4'>
+              {httmlContent}
+            </div>
+          </div>
+        )
+      }
     </div>
 
   );
