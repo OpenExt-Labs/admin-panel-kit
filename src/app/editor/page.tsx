2403b55569d429adc7a52e703893b5c2ed22cc27
sanglq3 <sanglq3@vng.com.vn>
2023-11-28 15:10:52 +0700

    add installation readme for tiptap

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -9,6 +9,12 @@ function TipTapEditor() {
         <h1 className="scroll-m-20 text-4xl font-bold tracking-tight">TipTap Editor</h1>
         <h2 className='font-heading mt-12 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0'>Installation</h2>
 
+        <h3 className='font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight'>Tiptap</h3>
+        <p className='mt-4 scroll-m-20'>
+          Tiptap is a headless wrapper around ProseMirror – a toolkit for building rich text WYSIWYG editors, which is already in use at many well-known companies such as New York Times, The Guardian or Atlassian.
+        </p>
+        <CodeFragment code={`yarn add @tiptap/react @tiptap/pm @tiptap/starter-kit`} />
+
         <h3 className='font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight'>Sass</h3>
         <p className='mt-4 scroll-m-20'>
           We use Sass for styling. You can install it with npm or yarn.
