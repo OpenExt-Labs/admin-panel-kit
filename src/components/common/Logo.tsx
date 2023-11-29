956ef7b9d4b3fcf7aa228009c27953bfc93663e2
sanglq3 <sanglq3@vng.com.vn>
2023-11-29 15:47:13 +0700

    udpate logo component

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -4,12 +4,9 @@ import Image from 'next/image'
 const Logo = () => {
   return (
     <div className='text-gray-600 py-2'>
-      <Link href='/'>
-        <div className='flex'>
-          <div className=''>
-            <Image src='/logo8.svg' alt='OpenExt logo' width={80} height={80} loading='lazy' />
-          </div>
-          {/* <span className='text-lg font-serif font-semibold my-auto' style={{ color: '#1A7F75' }}>OpenExt</span> */}
+      <Link href='/' className=''>
+        <div className='flex h-14 w-40 justify-center relative'>
+          <Image src='/logo9.svg' alt='OpenExt logo' fill loading='lazy' />
         </div>
       </Link>
     </div>
